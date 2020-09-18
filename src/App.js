fbba8ec9c2912469e76a04506d97bb0602810407
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-18 10:22:09 -0500

    created header and form component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header>
+        <h1>Lambda Eats</h1>
+        <p>You can remove this code and create your own header</p>
+        <nav>
+          <Switch>
+            <Route></Route>
+          </Switch>
+        </nav>
+      </header>
     </>
   );
 };
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header(props) {
+  return (
+    <header>
+      <h1>Lambda Eats</h1>
+      <nav></nav>
+    </header>
+  );
+}
