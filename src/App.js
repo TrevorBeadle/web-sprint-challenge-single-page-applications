91bb33af12691894d102eeac9ba8b0c8a82a5f9e
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-18 11:13:17 -0500

    started form, working on submit and change functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import Header from "./Header";
+import Form from "./Form";
+import axios from "axios";
+
+const initialForm = {
+  name: "",
+  size: "",
+  sauce: "",
+};
+
+const initialErrors = {
+  name: "",
+  size: "",
+  sauce: "",
+};
+
+const initialPizzas = [];
+const initialDisabled = true;
 
 const App = () => {
+  const [pizzas, setPizzas] = useState(initialPizzas);
+  const [formValues, setFormValues] = useState(initialForm);
+  const [formErrors, setFormErrors] = useState(initialErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const submit = () => {
+    const newPizza = {
+      name: "",
+      size: "",
+      sauce: "",
+    };
+  };
+
   return (
     <>
       <Switch>
         <Route path="/pizza">
           <Header />
+          <Form />
         </Route>
         <Route path="/">
           <Header />
--- src/Form.js
+++ src/Form.js
@@ -1,1 +1,58 @@
 import React from "react";
+
+export default function Form(props) {
+  const { values, submit, change, disabled, errors } = props;
+
+  const onSubmit = e => {
+    e.preventDefault();
+    submit();
+  };
+
+  const onChange = e => {
+    const { name, value, type, checked } = e.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h2>Build your own pizza!</h2>
+      <label htmlFor="name">
+        <input
+          type="text"
+          value={values.name}
+          onChange={onChange}
+          name="name"
+        />
+      </label>
+      <label htmlFor="size">
+        Choice of Size
+        <select name="size" value={values.size} onChange={onChange}>
+          <option value="">Select</option>
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+        </select>
+      </label>
+      <label>
+        Choice of Sauce
+        <input
+          type="radio"
+          name="sauce"
+          value="Original Red"
+          checked={values.sauce === "Original Red"}
+          onChange={onChange}
+        />
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="sauce"
+          value="Garlic Ranch"
+          checked={values.sauce === "Garlic Ranch"}
+          onChange={onChange}
+        />
+      </label>
+    </form>
+  );
+}
